3777d633cb998ef2eed89334211de8b53955cd2f
Andrey Kaldyaev <2033012@mail.ru>
2026-01-13 23:15:33 +0300

    fix: changed ios version, supported platforms

---
+++ Startribe Test App/Startribe_Test_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Startribe_Test_AppApp.swift
+//  Startribe Test App
+//
+//  Created by Andrey Kaldyaev on 13.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct Startribe_Test_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
